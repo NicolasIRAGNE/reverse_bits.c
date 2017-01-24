b07b7b8ec369d641aafbb99f2e6ce4b497d32335
Nicolas IRAGNE <nicoragne@hotmail.fr>
2017-01-24 16:43:56 +0100

    mieux

--- reverse_bits.c
+++ reverse_bits.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 15:43:36 by anonymous         #+#    #+#             */
-/*   Updated: 2017/01/24 16:02:16 by anonymous        ###   ########.fr       */
+/*   Updated: 2017/01/24 16:43:42 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,14 @@ unsigned char reverse(unsigned char octet)
 {
 	unsigned char tmp;
 	unsigned char tmp2;
-	unsigned char tmp3;
-	unsigned char tmp4;
 
-	tmp = (octet & 8) >> 3;
-	tmp2 = (octet & 4) >> 1;
-	tmp3 = (octet & 2) << 1;
-	tmp4 = (octet & 1) << 3;
-	tmp |= tmp2;
-	tmp |= tmp3;
-	tmp |= tmp4;
-	return (tmp);
+	tmp = ((octet & 8) >> 3 | ((octet & 4) >> 1));
+	tmp2 = ((octet & 2) << 1) | ((octet & 1) << 3);
+
+	return (tmp | tmp2);
 }
 
 unsigned char reverse_bits(unsigned char octet)
 {
-	unsigned char tmp;
-	unsigned char tmp2;
-	tmp = octet >> 4;
-	tmp2 = octet << 4;
-	tmp2 >>= 4;
-	return(reverse(tmp) | (reverse(tmp2) << 4));
+	return(reverse(octet >> 4) | (reverse((octet << 4) >> 4) << 4));
 }
