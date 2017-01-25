6913bc8b898e9d3abcf32a90d5d6419134cefff8
Nicolas IRAGNE <nicoragne@hotmail.fr>
2017-01-25 23:58:06 +0100

    better

--- reverse_bits.c
+++ reverse_bits.c
@@ -6,18 +6,17 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 15:43:36 by anonymous         #+#    #+#             */
-/*   Updated: 2017/01/24 16:43:42 by anonymous        ###   ########.fr       */
+/*   Updated: 2017/01/25 23:57:27 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned char reverse(unsigned char octet)
+unsigned char reverse(unsigned char half)
 {
 	unsigned char tmp;
 	unsigned char tmp2;
 
-	tmp = ((octet & 8) >> 3 | ((octet & 4) >> 1));
-	tmp2 = ((octet & 2) << 1) | ((octet & 1) << 3);
-
+	tmp = ((half & 8) >> 3 | ((half & 4) >> 1));
+	tmp2 = ((half & 2) << 1) | ((half & 1) << 3);
 	return (tmp | tmp2);
 }
 
